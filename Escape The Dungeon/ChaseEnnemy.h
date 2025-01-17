35c53212da1fc3b881d9c6da7a0649af3142322c
cbobeda <cbobeda@gaming.tech>
2025-01-17 17:20:24 +0100

    chase ennemy

--- Escape The Dungeon/ChaseEnnemy.h
+++ Escape The Dungeon/ChaseEnnemy.h
@@ -1,7 +1,18 @@
-﻿#pragma once
+﻿#include <SFML/Graphics.hpp>
 
-class ChaseEnnemy
+#include "Ennemy.h"
+
+class ChaseEnnemy : public Ennemy
 {
+    private:
+    sf::Texture texture;
+    sf::Sprite sprite;
+    int speed = 5;
+    int life = 5;
+    int maxLife = 5;
 public:
-    
+    ChaseEnnemy(int x, int y, sf::String chemin){texture.loadFromFile(chemin); sprite.setTexture(texture); sprite.setPosition(x, y);sprite.setScale(0.1,0.1);};
+    void update(float deltaTime,sf::Event* event) override;
+    void draw(sf::RenderWindow* window) override;
+    ~ChaseEnnemy(){};
 };
